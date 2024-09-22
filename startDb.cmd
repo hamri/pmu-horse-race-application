0bb34f796438953a50d9cbf585917bfc86fa722a
hamri <amri.hela@gmail.com>
2024-09-22 12:49:47 +0200

    Create startDb.cmd

---
+++ startDb.cmd
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Start an Postgre container (named flx-pg).
+# The db will be initialized with the init-user-db.sh script.
+
+CURRENT_DIR=$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/');
+PATH_TO_INIT="${CURRENT_DIR}\init-user-db.sh";
+
+docker run --name kemm-pg -e POSTGRES_PASSWORD=password -d -p 5432:5432 -v "${PATH_TO_INIT}:/docker-entrypoint-initdb.d/init-user-db.sh" postgres:12-bullseye

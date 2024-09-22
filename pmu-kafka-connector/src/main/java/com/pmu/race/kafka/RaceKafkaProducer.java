7333dff28a3a0bdb69a5c4a772acb83df11ecee4
hamri <amri.hela@gmail.com>
2024-09-22 23:01:31 +0200

    Update RaceKafkaProducer.java

--- pmu-kafka-connector/src/main/java/com/pmu/race/kafka/RaceKafkaProducer.java
+++ pmu-kafka-connector/src/main/java/com/pmu/race/kafka/RaceKafkaProducer.java
@@ -1,4 +1,28 @@
 package com.pmu.race.kafka;
 
-public class RaceKafkaProducer {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+public class RaceKafkaProducer<T> {
+    private final KafkaTemplate<String, T> kafkaTemplate;
+
+  public KemmKafkaProducer(final KafkaTemplate<String, T> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void produceEvent(final String eventKey, final T event, final String topicName,
+                           Runnable onSuccess, Consumer<Throwable> onFailed) {
+
+    kafkaTemplate.send(topicName, eventKey, event).whenComplete((sendResult, throwable) -> {
+      if (throwable != null) {
+        onFailed.accept(throwable);
+      } else {
+        onSuccess.run();
+      }
+    });
+
+  }
 }

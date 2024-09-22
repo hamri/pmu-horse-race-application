11eb5372f00d6c15156b7f75683767f77fd80616
HAmri <amri.hela@gmail.com>
2024-09-22 17:47:44 +0200

    Init Dependencies

---
+++ pmu-application-starter/src/main/java/com/pmu/race/manager/api/HorseRaceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pmu.race.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRaceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseRaceManagerApplication.class, args);
+	}
+
+}
---
+++ pmu-database-connector/pmu-jpa-model/src/main/java/com/pmu/race/db/model/HorseEntity.java
@@ -0,0 +1,4 @@
+package com.pmu.race.db.model;
+
+public class HorseEntity {
+}
